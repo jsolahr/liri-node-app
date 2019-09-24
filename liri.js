fd364698f1d0c106f0e5e5347db969ced9aa17d6
jsolahr <51134556+jsolahr@users.noreply.github.com>
2019-09-24 09:51:41 -0500

    installing npms

--- liri.js
+++ liri.js
@@ -1,1 +1,9 @@
 require("dotenv").config();
+
+//Require NPM Installs 
+var fs = require("fs");
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+var Spotify =require("node-spotify-api");
+var command = process.argv[2];
+var parameter = process.argv[3];
